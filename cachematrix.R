e66aef5b0d73a75a3a93211c1edbf2b7fc0d58fb
Trygve Buanes <st01355@ift041123.klientdrift.uib.no>
2015-06-12 11:08:18 +0200

    Implemented and tested makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## Make object which holds a matrix and it's inverse
+## Inverse is not calculated when creating the object, but should be set by
+## calling function
+## If input matrix is not square, the function returns NULL
+## Return makeCacheMatrix object
 makeCacheMatrix <- function(x = matrix()) {
-
+    # Check if matrix is square
+    if ( nrow(x)!=ncol(x) ) {
+        message( "Matrix is not square, returning NULL" )
+        return(NULL)
+    }
+    
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inv) inverse <<- inv
+    getInverse <- function() inverse
+    list( set=set, get=get, setInverse=setInverse, getInverse=getInverse )   
 }
 
 
-## Write a short comment describing this function
-
+## Find inverse of matrix of type makeCacheMatrix
+## Only calculate inverse if this has not been cached.
+## Cache inverse if not previously done
+## Return inversed matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if ( !is.null(inverse) ) {
+        message("Inverse read from cache")
+        return(inverse)
+    }
+    message("Inverse not cached, calculating it...")
+    data <- x$get()
+    inverse <- solve(data)
+    x$setInverse(inverse)
+    inverse      
 }
